379510cb24b1cd566f605eb521b6638369a64951
Carba <Carba@Porttil>
2019-01-23 19:41:12 +0100

    Primer cambio hola mundo 2

---
+++ HolaMundo2/src/holamundo2/HolaMundo2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo2;
+
+/**
+ *
+ * @author Carba
+ */
+public class HolaMundo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo 2");
+    }
+    
+}
