d2c69c2f73000d088d6f6b9da68538dab8d0f0d4
Carba <Carba@Porttil>
2019-01-23 19:48:39 +0100

    Segundo cambio hola mundo

--- src/holamundo1/HolaMundo1.java
+++ src/holamundo1/HolaMundo1.java
@@ -16,6 +16,7 @@ public class HolaMundo1 {
      */
     public static void main(String[] args) {
         System.out.println("Holis mundo");
+        System.out.println("Adiosmundo");
     }
     
 }
